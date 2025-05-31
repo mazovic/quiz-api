7d06031b6d3d4d5be951aab3b93eaca50c6d47c7
mazovic <mazovic23@gmail.com>
2025-05-31 16:11:57 +0300

    random

--- src/routes/mainRouter.ts
+++ src/routes/mainRouter.ts
@@ -1,10 +1,9 @@
-import express, { Router, Request, Response } from 'express';
+import express, { Router } from 'express';
 import authenticationRouter from '../authentication/routers/mainRouter';
 import userRouter from '../user/routers/mainRouter';
 import addressRouter from '../address/routers/mainRouter';
 import categoryRouter from '../categories/routers/mainRouter';
-import { logger } from '../utils/logger';
-import { seedQuestions } from '../seeder';
+import questionRouter from '../question/routers/mainRouter';
 
 const mainRouter: Router = express.Router();
 
@@ -16,14 +15,6 @@ mainRouter.use('/address', addressRouter);
 
 mainRouter.use('/categories', categoryRouter);
 
-mainRouter.post('/seed', async (req: Request, res: Response) => {
-	try {
-		await seedQuestions();
-		res.status(200).json({ status: 'ok' });
-	} catch (error) {
-		logger.error('Error seeding questions:', error);
-		res.status(500).json({ status: 'error', message: 'Failed to seed questions' });
-	}
-});
+mainRouter.use('/questions', questionRouter);
 
 export default mainRouter;
